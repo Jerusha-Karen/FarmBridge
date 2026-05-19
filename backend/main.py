69e1ee8169a3f00a7a4e4e13113538ee62b734fa
Karen <karen.indupallu@moveinsync.com>
2026-05-19 16:55:29 +0530

    fix cors and api url

--- backend/main.py
+++ backend/main.py
@@ -13,8 +13,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # will restrict after deployment
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
